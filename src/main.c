5680099b30c4a4f820061cdb45815114fdaf5e91
Julian Duque <julian.duque.alayon@gmail.com>
2025-12-16 19:21:13 +0100

    simple main

--- src/main.c
+++ src/main.c
@@ -5,15 +5,5 @@ int main(void) {
     printf("Hola, C!\n");
     printf("Plantilla lista para proyectos serios.\n");
     
-    int *ptr = malloc(sizeof(int));
-    if (ptr == NULL) {
-        fprintf(stderr, "Error: no se pudo asignar memoria\n");
-        return EXIT_FAILURE;
-    }
-    
-    *ptr = 42;
-    printf("Valor: %d\n", *ptr);
-    
-    free(ptr);
     return EXIT_SUCCESS;
 }
